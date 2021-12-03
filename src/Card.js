cfe9302337561b584e46d5ec8186b8b4bf1719a0
Aashu-007 <gnirmal16@gmail.com>
2021-12-03 17:13:18 +0530

    cards shadow

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 //import Card from '@mui/material/Card';
 //import CardActions from '@mui/material/CardActions';
 //import CardContent from '@mui/material/CardContent';
@@ -6,27 +6,27 @@ import * as React from 'react';
 //import Button from '@mui/material/Button';
 //import Typography from '@mui/material/Typography';
 
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
@@ -39,9 +39,8 @@ export default function RecipeReviewCard() {
   };
 
   return (
-    <Card sx={{ maxWidth: 300}}>
-      <CardHeader 
-        
+    <Card sx={{ maxWidth: 300 }}>
+      <CardHeader
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
@@ -58,7 +57,7 @@ export default function RecipeReviewCard() {
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella 
+          This impressive paella
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -81,17 +80,18 @@ export default function RecipeReviewCard() {
         <CardContent>
           <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+            set aside for 10 minutes.
           </Typography>
           <Typography paragraph>
             Heat oil in a (14- to 16-inch) paella pan or a large,
           </Typography>
           <Typography paragraph>
-            Add rice and stir very gently to distribute. 
+            Add rice and stir very gently to distribute.
           </Typography>
           <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+            Set aside off of the heat to let rest for 10 minutes, and then
+            serve.
           </Typography>
         </CardContent>
       </Collapse>
--- src/Fetch.js
+++ src/Fetch.js
@@ -1,70 +1,79 @@
-import React from 'react';
-import firebase from './firebase';
-import {useEffect,useState} from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import React from "react";
+import firebase from "./firebase";
+import { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CardActionArea from "@mui/material/CardActionArea";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const GetData = () => {
-
 	const [plantData, setPlantData] = useState([]);
 
 	useEffect(() => {
-		const firestore=firebase.database().ref("/PlantDatabase");
-		firestore.on('value',(response)=>{
+		const firestore = firebase.database().ref("/PlantDatabase");
+		firestore.on("value", (response) => {
 			const data = response.val();
 			let PlantsInfo = [];
-			for(let id in data){
+			for (let id in data) {
 				PlantsInfo.push({
-					id:id,
-					ImgUrl:data[id].ImgUrl,
-					LocalName:data[id].LocalName,
-					ScientificName:data[id].ScientificName,
-					Distribution:data[id].Distribution,
-					PartUseandUses:data[id].PartUseandUses,
+					id: id,
+					ImgUrl: data[id].ImgUrl,
+					LocalName: data[id].LocalName,
+					ScientificName: data[id].ScientificName,
+					Distribution: data[id].Distribution,
+					PartUseandUses: data[id].PartUseandUses,
 					Types: data[id].Types,
 				});
 			}
 			setPlantData(PlantsInfo);
-		})
+		});
 	}, []);
 
 	return (
-		<div className="float">
-		<p>Data fetched : 
-
-		{
-			plantData.map((data,index)=>{
-				return <>
-				<Card sx={{ maxWidth: 300 }}>
-					<CardMedia
-        			component="img"
-        			alt="plant"
-        			height="180"
-        			image={data.ImgUrl}
-      			/>
-					<CardContent>
-        				<Typography gutterBottom variant="h6" component="div">
-          				{data.LocalName}
-       					</Typography>
-        				<Typography variant="body2" color="text.secondary">
-          				{data.PartUseandUses}
-        				</Typography>
-      				</CardContent>
-      				<CardActions>
-        				<Button size="small">View</Button>
-        				<Button size="small">Learn More</Button>
-      				</CardActions>
-				</Card>
-				</>
-			})
-		}
-		</p>
+		<div>
+			<p>
+				Data fetched :
+				{plantData.map((data, index) => {
+					return (
+						<><br/>
+							<Card sx={{ maxWidth: 300 }} raised="true">
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt="plant"
+										height="180"
+										image={data.ImgUrl}
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h6"
+											component="div"
+										>
+											{data.LocalName}
+										</Typography>
+										<Typography
+											variant="body2"
+											color="text.secondary"
+										>
+											{data.PartUseandUses}
+										</Typography>
+									</CardContent>
+									<CardActions>
+										<Button size="small">View</Button>
+										<Button size="small">Learn More</Button>
+									</CardActions>
+								</CardActionArea>
+							</Card>
+						</>
+					);
+				})}
+			</p>
 		</div>
-);}
+	);
+};
 
 export default GetData;
