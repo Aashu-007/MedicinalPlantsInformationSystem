c23b83d600b009dfb32925cda7a902d517c0a794
Aashu-007 <gnirmal16@gmail.com>
2021-12-22 12:48:27 +0530

    removed old card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "./App.css";
 import Fetch from "./Fetch";
-import { Offline, Online } from "react-detect-offline";
+// import { Offline, Online } from "react-detect-offline";
 import Scroll from "./Scroll";
 import AppBar from "./Appbar";
 
 function App() {
   return (
     <div>
-      <Offline>Make sure you have an active internet connection.</Offline>
-
-       <Online>
-      {/*<Drawer />*/}
-      <AppBar />
-      <Fetch />
-      <Scroll showBelow={140} />
-      </Online>
+      {/*<Offline>Make sure you have an active internet connection.</Offline>*/}
+      {/*<Online>*/}
+        {/*<Drawer />*/}
+        <AppBar />
+        <Fetch />
+        <Scroll showBelow={145} />
+      {/*</Online>*/}
     </div>
   );
 }
