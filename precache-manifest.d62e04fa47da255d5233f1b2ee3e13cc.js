12c2a1116d8d6414555ea3828d78007f363d47f3
Aashu-007 <gnirmal16@gmail.com>
2022-01-18 12:56:15 +0530

    Updates

---
+++ precache-manifest.d62e04fa47da255d5233f1b2ee3e13cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fe8a5314450045b2ec48ff85d14cac",
+    "url": "/MedicinalPlantsInformationSystem/index.html"
+  },
+  {
+    "revision": "263be9d45b23e08d9307",
+    "url": "/MedicinalPlantsInformationSystem/static/css/2.a63ca25f.chunk.css"
+  },
+  {
+    "revision": "8998fe89f68669f2f549",
+    "url": "/MedicinalPlantsInformationSystem/static/css/main.e49d2551.chunk.css"
+  },
+  {
+    "revision": "263be9d45b23e08d9307",
+    "url": "/MedicinalPlantsInformationSystem/static/js/2.66cff8ae.chunk.js"
+  },
+  {
+    "revision": "8a4ac74a8b97cfd5736f68f8514e6d9e",
+    "url": "/MedicinalPlantsInformationSystem/static/js/2.66cff8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8998fe89f68669f2f549",
+    "url": "/MedicinalPlantsInformationSystem/static/js/main.4317c635.chunk.js"
+  },
+  {
+    "revision": "74dda3508726cf51dcb6",
+    "url": "/MedicinalPlantsInformationSystem/static/js/runtime-main.e77b4918.js"
+  }
+]);
